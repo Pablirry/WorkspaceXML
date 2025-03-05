bdeff2921a3830c5a74982990adcf0acb4c4f992
Pablirry <pablo.lopez02@csioviedo.com>
2025-03-05 19:37:35 +0100

    .

--- Ejemplo1/src/Ejemplo/Main.java
+++ Ejemplo1/src/Ejemplo/Main.java
@@ -3,14 +3,23 @@ package Ejemplo;
 import java.io.*;
 
 import javax.xml.parsers.*;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		leerDom();
+		escribirDom();
 
 	}
 
@@ -19,11 +28,11 @@ public class Main {
 		DocumentBuilder db;
 		try {
 			db = dbf.newDocumentBuilder();
-			File file = new File("Ejemplo1/coches.xml");
+			File file = new File("coches.xml");
 			Document doc = db.parse(file);
-			
+
 			doc.getDocumentElement().normalize();
-			
+
 			NodeList nodeList = doc.getElementsByTagName("coche");
 
 			for (int i = 0; i < nodeList.getLength(); i++) {
@@ -43,9 +52,7 @@ public class Main {
 					System.out.println("Cilindrada: " + cilindrada);
 					System.out.println();
 				}
-				
 			}
-			
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -56,7 +63,65 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
+	private static void escribirDom() {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.newDocument();
+
+			Element root = doc.createElement("concesionario");
+			doc.appendChild(root);
+
+			agregarCoche(doc, root, "5", "seat", "ibiza", "1.4");
+			agregarCoche(doc, root, "6", "seat", "leon", "1.6");
+			agregarCoche(doc, root, "7", "seat", "ateca", "2.0");
+
+			TransformerFactory tf = TransformerFactory.newInstance();
+			Transformer t = tf.newTransformer();
+			t.setOutputProperty(OutputKeys.INDENT, "yes");
+
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("nuevo_coches.xml"));
+
+			t.transform(source, result);
+			System.out.println("Fichero creado");
+
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void agregarCoche(Document doc, Element root, String id, String marca, String modelo,
+			String cilindrada) {
+
+		Element coche = doc.createElement("coche");
+		coche.setAttribute("id", id);
+
+		Element marcaElement = doc.createElement("marca");
+		marcaElement.appendChild(doc.createTextNode(marca));
+		coche.appendChild(marcaElement);
+
+		Element modeloElement = doc.createElement("modelo");
+		modeloElement.appendChild(doc.createTextNode(modelo));
+		coche.appendChild(modeloElement);
+
+		Element cilindradaElement = doc.createElement("cilindrada");
+		cilindradaElement.appendChild(doc.createTextNode(cilindrada));
+		coche.appendChild(cilindradaElement);
+
+		root.appendChild(coche);
+
+	}
 }
